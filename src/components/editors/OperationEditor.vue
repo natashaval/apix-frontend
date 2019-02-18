32e3cb62482fe2ebbe153a6ed5f4a610eec3a187
alfian <alfian853@gmail.com>
2019-02-18 18:58:44 +0700

    update

--- src/components/editors/OperationEditor.vue
+++ src/components/editors/OperationEditor.vue
@@ -1,21 +1,20 @@
 <template>
-    <div>
+    <div v-if="apiData !== undefined">
         <h1>method : {{operationApi}}</h1>
-        <DataTypeInput/>
-        <!--<BodyForm v-if="apiData.body!=null" :apiData="apiData.body"/>-->
+        <DataTypeInput :apiData="apiData"/>
     </div>
 </template>
 
 <script>
-    // import BodyForm from "./editor-components/inputs/BodyForm";
     import DataTypeInput from "./editor-components/inputs/DataTypeInput";
+
     export default {
         name: "OperationEditor",
         components: {DataTypeInput},
         props : ['projectId','sectionApi','pathApi','operationApi'],
         computed : {
             apiData : function(){
-                return this.$store.getters['project/getOperationData'](this.sectionApi,this.pathApi,this.operationApi)
+                return this.$store.getters['project/getPathData'](this.sectionApi,this.pathApi,this.operationApi)
             }
         }
     }
