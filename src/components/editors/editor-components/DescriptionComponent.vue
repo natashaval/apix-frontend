1371b4d7cf95e1dc385d0aa9878e327571a98ce1
natashaval <natasha.valentina1998@gmail.com>
2019-02-22 23:52:48 +0700

    Add section general

--- src/components/editors/editor-components/DescriptionComponent.vue
+++ src/components/editors/editor-components/DescriptionComponent.vue
@@ -5,15 +5,13 @@
         <InputEditor :apiData="apiData.info.title" :apiTitle="`title`" :isEditing="isEditing"></InputEditor>
         <InputEditor :apiData="apiData.info.version" :apiTitle="`version`" :isEditing="isEditing"></InputEditor>
         <TextDocInput id="info-description" :apiData="apiData.info.description" :apiTitle="`Description`" :isEditing="isEditing"/>
-        <InputEditor :apiData="apiData.info.contact.name" :apiTitle="`Contact Name`" :isEditing="isEditing"></InputEditor>
-        <InputEditor :apiData="apiData.info.contact.email" :apiTitle="`Contact Email`" :isEditing="isEditing"></InputEditor>
-        <InputEditor :apiData="apiData.info.contact.url" :apiTitle="`Contact URL`" :isEditing="isEditing"></InputEditor>
-        <InputEditor :apiData="apiData.info.license.name" :apiTitle="`License Name`" :isEditing="isEditing"></InputEditor>
-        <InputEditor :apiData="apiData.info.license.url" :apiTitle="`License URL`" :isEditing="isEditing"></InputEditor>
+        <InputEditor v-for="(value,key) in apiData.info.contact" v-bind:key="`info-${key}`" :apiData="value" :apiTitle="`Contact ${key}`" :isEditing="isEditing"/>
+        <InputEditor v-for="(value,key) in apiData.info.license" v-bind:key="`info-${key}`" :apiData="value" :apiTitle="`License ${key}`" :isEditing="isEditing"/>
         <TextDocInput id="info-tos" :apiData="apiData.info.termsOfService" :apiTitle="`Terms of Service`" :isEditing="isEditing"/>
 
         <h3>ExternalDocs</h3>
-        <InputEditor v-for="(value,key) in apiData.externalDocs" v-bind:key="key" :apiData="value" :apiTitle="key"></InputEditor>
+        <InputEditor :apiData="apiData.externalDocs.url" :apiTitle="`url`" :isEditing="isEditing"></InputEditor>
+        <TextDocInput id="externaldocs-description" :apiData="apiData.externalDocs.description" :apiTitle="`description`" :is-editing="isEditing" />
     </div>
 </template>
 
