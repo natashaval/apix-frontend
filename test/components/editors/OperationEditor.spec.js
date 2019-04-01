115c811a27b67ccd466bf459825445d66a98d1aa
alfian <alfian853@gmail.com>
2019-04-01 13:56:47 +0700

    add new dependency for unit test

---
+++ test/components/editors/OperationEditor.spec.js
@@ -0,0 +1,8 @@
+import {mount}  from '@vue/test-utils';
+import ActionBuilder from '@/utils/ActionBuilderUtil';
+
+describe('App', () => {
+    test('should mount for testing', () => {
+        expect(1).toEqual(1);
+    });
+});
