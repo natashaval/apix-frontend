8bc008d6837fc74d7948b8167897b4a2fa62b57f
alfian <alfian853@gmail.com>
2019-04-02 23:38:27 +0700

    reconfig, add new dependency

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,20 @@
 module.exports = {
   presets: [
     '@vue/app'
-  ]
+  ],
+  "env": {
+    "test": {
+      "presets": [
+        [
+          "@babel/preset-env",
+          {
+            "targets": {
+              "node": "current"
+            }
+          }
+        ]
+      ]
+    }
+  }
+
 }
