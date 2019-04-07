fbcbe79de3718a86bae1c67cf85d2ce7715de6b1
natashaval <natasha.valentina1998@gmail.com>
2019-04-07 16:59:39 +0700

    add axios interceptor

--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
 import {store} from './stores'
-import {router} from "./routers/router";
+import {router} from "./routers/router"
+import axios from 'axios'
 
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
@@ -10,6 +11,14 @@ import BootstrapVue from 'bootstrap-vue'
 Vue.use(BootstrapVue)
 
 Vue.config.productionTip = false
+const apixToken = localStorage.getItem('apix-token')
+
+axios.interceptors.request.use(function (config) {
+  if (apixToken) {
+    axios.defaults.headers.common['Authorization'] = `Bearer ${apixToken}`
+  }
+  return config
+})
 
 new Vue({
   router,
