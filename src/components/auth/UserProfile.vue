d8c156db7e9165778392e6bafb4e9af4f4860e51
natashaval <natasha.valentina1998@gmail.com>
2019-04-29 12:26:58 +0700

    filter team based on grant(not yet card(

--- src/components/auth/UserProfile.vue
+++ src/components/auth/UserProfile.vue
@@ -12,23 +12,32 @@
         <div>
             <b-tabs>
                 <b-tab title="Team" active>
+
+                    <button @click="dumpTeam">{{dump}}</button>
+                    <p v-if="dump">TRUE {{grantUser}}</p>
+                    <p v-if="!dump"> FALSE {{ ungrantUser }}</p>
                     <!--b-list-group v-for="team in profile.teams" :key="team">
                         <b-list-group-item :to="{name: '', params: {}} ">{{team}}</b-list-group-item>
                     </b-list-group-->
-                    <div class="mt-3">
+
+                    <!--div class="mt-3" v-if="ungrantUser.length">
                         <b-card-group deck class="mb-3">
-                            <b-card v-for="team in profile.teams" :key="team"
-                                    :title="team" cols="4">
-                                <div slot="footer">
-                                    <b-button :href="team" variant="primary">Enter</b-button>
-                                </div>
-                            </b-card>
+                            INI YANG GRANT FALSE (perlu confirm invitation)
+                            {{ ungrantUser }}
                         </b-card-group>
                     </div>
+
+                    <div class="mt-3" v-if="grantUser.length">
+                        INI YANG GRANT tRUE
+                        <b-card-group deck class="mb-3" v-for="team in grantUser" :key="team.id">
+                            <TeamCard :team="team"></TeamCard>
+                        </b-card-group>
+                        {{ grantUser }}
+                    </div-->
                 </b-tab>
-                <b-tab title="Version">
+                <!--b-tab title="Version">
                     Ongoing version
-                </b-tab>
+                </b-tab-->
             </b-tabs>
         </div>
     </div>
@@ -36,16 +45,38 @@
 
 <script>
     import {AUTH_LOGOUT} from "../../stores/actions/auth";
+    import {BASE_URL} from "../../stores/actions/const";
+    import axios from 'axios'
+    import TeamCard from "./team-components/TeamCard";
+
 
     export default {
         name: "UserProfile",
+        components: {TeamCard},
+        data: function(){
+            return {
+                teams: null,
+                dump: false
+            }
+        },
         computed: {
             profile() {
                 return this.$store.getters['user/getProfile']
             },
             isAuthenticated () {
                 return this.$store.getters['auth/isAuthenticated']
-            }
+            },
+            grantUser: function() {
+                return  this.teams.filter(u =>
+                    u.members.some((members) => members.grant && members.username.includes(this.profile.username))
+                );
+            },
+            ungrantUser: function () {
+                return this.teams.filter((u) =>
+                    u.members.some((members) => !members.grant && members.username.includes(this.profile.username))
+                );
+            },
+
         },
         methods: {
             setLayout (layout) {
@@ -57,7 +88,22 @@
                 }, (err) => {
                     console.log('login', err)
                 })
+            },
+            dumpTeam(){
+                this.dump = !this.dump;
             }
+        },
+        mounted: function() {
+            this.$nextTick(function () {
+                this.setLayout('all-layout');
+            });
+        },
+        created() {
+            axios.get(BASE_URL + "teams/my-team").then((response) => {
+                this.teams = response.data
+            }).catch((e) => {
+                console.error(e)
+            })
         }
     }
 </script>
---
+++ src/components/auth/team-components/TeamCard.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <b-card cols="4" class="mb-2"
+                :title="team.name" >
+            {{ team }}
+            <div slot="footer">
+                <b-button :href="team" variant="primary">Confirm</b-button>
+            </div>
+        </b-card>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "TeamCard",
+        props: ['team']
+    }
+</script>
+
+<style scoped>
+
+</style>
