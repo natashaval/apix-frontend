4f9ab4fcba14851dabe3c41ccc7c4fe7357d3ed7
natashaval <natasha.valentina1998@gmail.com>
2019-07-09 10:11:18 +0700

    github editor fixing editor

--- src/components/editors/GithubEditor.vue
+++ src/components/editors/GithubEditor.vue
@@ -11,10 +11,16 @@
                         class="float-right round-button btn mt-2 mr-2" v-bind:id="_uid+'-edit-btn'">
                     <i class="fa fa-pencil-alt"></i>
                 </button>
+                <button @click="fetchOas" class="float-right btn mt-2 mr-2" type="button"
+                        data-toggle="tooltip" data-placement="bottom" title="Sync with Github"
+                >
+                    <i class="fas fa-sync-alt"></i>
+                </button>
             </div>
         </div>
         <div class="row">
             <div v-if="showEdit" class="col-11" style="border-color: crimson">
+                <!--{{ownerData}}-->
                 <div class="form-group" v-if="ownerData">
                     <label class="font-weight-bold">Owner : &nbsp;</label>
                     <span class="badge badge-secondary">
@@ -57,14 +63,19 @@
                         <button class="btn btn-outline-dark ml-5" @click="push">Push to Github</button>
                     </div>
                 </div>
-                <div v-if="commitResponse.commitDate">Has been updated!
-                    {{commitResponse}}
+                <div clas="row">
+                    <div class="col-8" v-if="commitResponse.commitDate">
+                        <div class="alert alert-primary" role="alert">
+                            Project is successfully push to Github -> {{commitResponse.message}}
+                        </div>
+                    </div>
                 </div>
             </div>
 
             <div v-else class="col-11">
                 <div class="row">
-                    <div class="col-md-12">
+                    <!--{{ ownerData }}-->
+                    <div class="col-md-12" v-if="ownerData">
                         <p class="font-weight-bold">Owner :  <span class="badge badge-secondary">{{owner}}</span></p> <br />
                     </div>
                 </div>
@@ -78,14 +89,20 @@
                 </div>
                 <div class="row">
                     <div class="col-md-12">
-                        <p class="font-weight-bold">Path: {{ path }}</p>
+                        <p class="font-weight-bold">Path: {{ path }} &nbsp;
+                        <span><i v-show="oasLoading" class="fa fa-spinner fa-spin"></i></span>
+                        </p>
                     </div>
                 </div>
             </div>
 
-            <vue-editor disabled v-model="content.content"></vue-editor>
-
         </div>
+        <div class="row" v-if="content">
+            <div class="col-md-12">
+                <vue-editor disabled v-model="content.content"></vue-editor>
+            </div>
+        </div>
+
 
     </div>
 </template>
@@ -166,8 +183,9 @@
             loadData: function(){
                 this.$_changeObserverMixin_unObserve();
                 this.projectId = this.$route.params.projectId;
-                if (this.githubData !== null) {
-                    // if (this.githubData.owner !== '' && this.githubData.repo !== ''){
+                // if (this.githubData !== null) {
+                if (this.githubData.owner !== '' && this.githubData.repo !== ''){
+                    console.log('github ada')
                     this.owner = this.githubData.owner;
                     this.repo = this.githubData.repo;
                     this.branch = this.githubData.branch;
@@ -176,6 +194,7 @@
                 }
                 else {
                     if (this.isOwner){
+                        console.log('ada owner')
                         this.owner = this.ownerData.login
                     }
                     else {console.log('there nothing you can do')}
@@ -257,21 +276,28 @@
 
             },
             push: function () {
+                let branchPush = ''
+                if (this.branch == '') branchPush = 'master'
+                else branchPush = this.branch
                 axios.put(BASE_URL + '/github/api/repos/' + this.owner + '/' + this.repo + '/contents/' + this.path,
                     {
                         message: this.message,
                         projectId: this.projectId,
-                        sha: this.content.sha
+                        sha: this.content.sha,
+                        branch: branchPush
                     })
                     .then((response) => {
                         this.commitResponse = response.data
                         console.log(this.commitResponse)
-                        this.makeToast('success', response.data.success, "Success push to github: " + response.data.message);
-                        this.fetchOas();
+                        this.makeToast('success', response.data.success, "Success push to github -> " + response.data.message);
+                        this.$nextTick(() => {
+                            this.fetchOas();
+                        })
+
                     })
                     .catch((e) => {
                         console.error(e);
-                        this.makeToast('danger', e.response.data.success, e.response.data.message);
+                        this.makeToast('danger', e.response.data.success, e.response.data.message + e.response.data.errors[0]);
                     })
             },
             fetchBranchList: function () {
@@ -290,7 +316,9 @@
                 }
             },
             fetchOas: function() {
-                if (this.githubData !== undefined) {
+                // if (this.githubData !== undefined) {
+                if (this.githubData.repo !== ''){
+                    console.log('fetch oas')
                     this.oasLoading = true;
                     axios.get(BASE_URL + '/github/api/repos/' + this.owner + '/' + this.repo + '/contents/' + this.path,
                         {
@@ -342,6 +370,7 @@
         watch: {
             $route: function () {
                 this.loadData();
+                this.fetchOas();
             }
         },
         mounted() {
@@ -352,10 +381,10 @@
 
 <style scoped>
     /*https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_autocomplete*/
-.autocomplete {
-    position: relative;
-    /*display: inline-block;*/
-}
+    .autocomplete {
+        position: relative;
+        /*display: inline-block;*/
+    }
     .autocomplete-items {
         position: absolute;
         background-color: white;
